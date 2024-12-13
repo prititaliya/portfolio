f01a2fd4dc30a803fc26b911ea0c888e3042ef31
Prit Italiya <71205033+prititaliya@users.noreply.github.com>
2024-12-13 11:42:26 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -91,3 +91,12 @@ lightModeBtn.addEventListener("click", () => {
   lightModeBtn.classList.remove("active");
   document.documentElement.setAttribute('color-scheme', 'dark');
 });
+
+
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('nav ul');
+
+hamburger.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+  hamburger.classList.toggle('active');
+});
